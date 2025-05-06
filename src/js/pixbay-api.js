3c3e327da514c0276610492142d83a1c0afb6865
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-05-06 14:48:48 +0100

    Switch to async await

--- src/js/pixbay-api.js
+++ src/js/pixbay-api.js
@@ -3,26 +3,6 @@ import axios from 'axios';
 const API_KEY = '49754838-8d67938118f09bcabafca9026';
 const BASE_URL = 'https://pixabay.com/api/';
 
-// Get images by query using api
-// Link example: pixabay.com/api/?key=49754838-8d67938118f09bcabafca9026&q=dog&image_type=photo&orientation=horizontal
-// export default function getImagesByQuery(query) {
-//     return axios
-//         .get(BASE_URL, {
-//             params: {
-//                 key: API_KEY,
-//                 q: query,
-//                 image_type: 'photo',
-//                 orientation: 'horizontal',
-//                 per_page: 50,
-//             },
-//         })
-//         .then((response) => response.data.hits)
-//         .catch((error) => {
-//             console.error('❌ Failed to fetch images:', error.message);
-//             throw error;
-//         });
-// }
-
 export default async function getImagesByQuery(query) {
     const response = await axios.get(BASE_URL, {
         params: {
