6a48d5776e9a4362f94ee06ec0be5e591fe5fc18
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-05-06 15:22:58 +0100

    Make params variable

--- src/js/pixbay-api.js
+++ src/js/pixbay-api.js
@@ -4,14 +4,14 @@ const API_KEY = '49754838-8d67938118f09bcabafca9026';
 const BASE_URL = 'https://pixabay.com/api/';
 
 export default async function getImagesByQuery(query) {
-    const response = await axios.get(BASE_URL, {
-        params: {
-            key: API_KEY,
-            q: query,
-            image_type: 'photo',
-            orientation: 'horizontal',
-            per_page: 50,
-        },
-    });
+    const params = {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        per_page: 15,
+        page: 1,
+    };
+    const response = await axios.get(BASE_URL, { params });
     return response.data.hits;
 }
