334650da20c72af7d29186b0067e70afc8a78907
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-05-07 13:00:24 +0100

    Fix  infinit scroll

--- src/main.js
+++ src/main.js
@@ -9,14 +9,17 @@ import {
 } from './js/render-functions';
 
 const form = document.querySelector('.form');
-
+// const formData = new FormData(form);
+// const searchQuery = formData.get('search-text').trim();
+let searchQuery;
 const observerTarget = document.querySelector('.js-guard');
 
 let currantPage = 1;
+const perPage = 15;
 let hits = 0;
-let isLoading = false;
-let hasMore = true;
-let options = {
+// let isLoading = false;
+// let hasMore = true;
+const options = {
     rootMargin: '350px',
 };
 
@@ -26,14 +29,41 @@ async function onScroll(entries, observer) {
     const entry = entries[0];
     if (entry.isIntersecting) {
         console.log('Вжух Вантаж');
+        observer.unobserve(observerTarget);
+
+        currantPage += 1;
+
+        // console.log(currantPage);
+        try {
+            console.log(searchQuery);
+            const images = await getImagesByQuery(searchQuery, currantPage, perPage);
+
+            if (!images.hits.length) {
+                showWarningToast('Thats all');
+                return;
+            }
+            createGallery(images.hits);
+
+            hits += images.hits.length;
+            if (hits < images.totalHits) {
+                observer.observe(observerTarget);
+            }
+            // observer.observe(observerTarget);
+        } catch (error) {
+            showErrorToast('An error occurred while fetching images.');
+            console.error('Error fetching images:', error);
+        }
+        // finally {
+        //     hideLoader();
+        //     setBtnLoading(false);
+        // }
     }
-    // observer.unobserve(observerTarget);
 }
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
 
     const formData = new FormData(form);
-    const searchQuery = formData.get('search-text').trim();
+    searchQuery = formData.get('search-text').trim();
 
     if (!searchQuery) {
         showWarningToast('Please enter a search query.');
